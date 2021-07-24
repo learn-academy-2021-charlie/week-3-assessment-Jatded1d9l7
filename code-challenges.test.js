d9f76ae9734de018e722646fbabc646db1a78436
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-23 20:37:48 -0400

    small win finguring out how to filter numbers in the array

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,63 +1,129 @@
-// ASSESSMENT 3: Coding practical questions with Jest
+// // ASSESSMENT 3: Coding practical questions with Jest
+//
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
+//
+// // Add appropriate dependencies to the repository:
+// // $ yarn add jest
+//
+// // Use test driven development to complete the following questions
+// // Run the file with the following command:
+// // $ yarn jest
+//
+// // Reminder: The test will call your function
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
-
-// Add appropriate dependencies to the repository:
-// $ yarn add jest
-
-// Use test driven development to complete the following questions
-// Run the file with the following command:
-// $ yarn jest
-
-// Reminder: The test will call your function
-
-
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array of that length containing the numbers of the Fibonacci.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-// Example input: 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-// Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//Pseudo Code
+// function = fibonacci
+// Parameter = number
+// Returns = array
 
 
 
-// b) Create the function that makes the test pass.
+
+// // --------------------1) Create a function that takes in a number (greater than 2) and returns an array of that length containing the numbers of the Fibonacci.
+//
+// // a) Create a test with expect statements for each of the variables provided.
+
+// Test Driven Development TDD_______________________________________
+// describe("functionReturnArray", () => {
+//   test("returns a string the length of number in fibbonacci sequence", () => {
+//     expect(functionReturnArray()).toEqual([1, 1, 2, 3, 5, 8])
+//   })
+// })
+//TDD_________________________________________________________________
+
+//Pseudo Code
+// I am creating a function => () {}
+// I will call this function fibbonacci => () {}
+// To do this I must declare it with Const fibonacci => () {}
+// the function fibbanaci must take in a number as a parameter (number)
+// the number must be greater than 2
+// the function must return an array
+// the array must be the length containing the numbers of the fibbanaci sequence.
+
+// How to return an array?
+// How to evaluate the fibbonacci sequence?
+// Each number is the sum of the two preceding ones.
+
+//
+// var myNumber1 = 6
+// var myNumber1 = 10
+//
+// const fibbonacci = (number) {
+//   if number > 2
+//
+// }
 
 
+// // Example input: 6
+// // Expected output: [1, 1, 2, 3, 5, 8]
+//
+// // Example input: 10
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//
+//
+//
+// // b) Create the function that makes the test pass.
 
-// --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+//___________________________________________________________________
 
-// a) Create a test with expect statements for each of the variables provided.
+//Pseudo Code
+// Function that takes in an array
+// function = onlyOdds
+// sorted from least to greatest
+
+// // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+
+// // // a) Create a test with expect statements for each of the variables provided.
+// describe("onlyOdds", () => {
+//   test("returns a newArray of only odds", () => {
+//     expect(onlyOdds()).toEqual(-9, 7, 9, 199)
+//   })
+// })
+
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
 
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+let onlyNums = (arr) => {
+  return arr.filter(arr => typeof arr === "number")
+}
 
-
-// b) Create the function that makes the test pass.
-
-
-
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
-var numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-var numbersToAdd3 = []
-// Expected output: []
+console.log(onlyNums(fullArr1))
 
 
 
-// b) Create the function that makes the test pass.
+// the following Did not work
+// let onlyNums = (val) => {
+//   if (typeof(val) === 'number'){
+//     return val
+//   }
+// }
+// ^^ did not work
+
+// // Expected output: [-9, 7, 9, 199]
+//
+// var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// // Expected output: [-823, 7, 23]
+//
+//
+// // b) Create the function that makes the test pass.
+//
+//
+//
+// // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+//
+// // a) Create a test with expect statements for each of the variables provided.
+//
+// var numbersToAdd1 = [2, 4, 45, 9]
+// // Excpected output: [2, 6, 51, 60]
+//
+// var numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+//
+// var numbersToAdd3 = []
+// // Expected output: []
+//
+//
+//
+// // b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -9,39 +9,39 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: when working in react `this`, is used to indicate that the variable is a part of the overall function.
 
-  Researched answer:
+  Researched answer: `this` is a keyword that refers to the object it belongs to.
 
 
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a test driven development tool used to test code.
 
-  Researched answer:
+  Researched answer: Yarn is a package manager for code that allows users to share and use code with other users.
 
 
 
 3. What are props in React?
 
-  Your answer:
+  Your answer: Props are properties that are passed down to child components in a project.
 
-  Researched answer:
+  Researched answer: In react information can be passed to a component using props. Props can be thought of as a global variable or object.
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JSX is a blend of two languages; HTML and Javascript. It returns in HTML but can also use JavaScript functions using curly braces {}.
 
-  Researched answer:
+  Researched answer: JSX is a HML/HTML like syntax so XML/HTML text can co-exist with JS/React.
 
 
 
 5. What is a DOM event?
 
-  Your answer:
+  Your answer: DOM works in the background to refresh individual components of a page. DOM makes interacting with pages more efficient because the browser does not have to refresh the whole page.
 
   Researched answer:
 
@@ -49,19 +49,19 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function is one that is not directly called upon?
 
-  Researched answer:
+  Researched answer: Anonymous functions are functions without names.  They are not stored, but they are associated with a variable.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering refers to the process of delivering elements and components based on certain conditions.
 
-2. Ruby:
+2. Ruby: Ruby is a programming language that focuses on simplicity and productivity.  
 
-3. Object oriented programming:
+3. Object oriented programming: a programming paradigm that relies on the concept of classes and objects.
 
-4. Ruby hash:
+4. Ruby hash: Hash is a data structure that maintains a set of objects which are termed as the keys and each key associates a value with it. A hash is a collection of unique keys and their values. 
 
-5. Ruby blocks:
+5. Ruby blocks A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call.
