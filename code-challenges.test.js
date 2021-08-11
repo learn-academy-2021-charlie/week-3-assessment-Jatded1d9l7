e6234d9efb26536288864f8b437d417ff67530fc
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-08-11 13:46:32 -0400

    updated

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -6,21 +6,21 @@
 
 //-------------------- CODE BLOCK START ----------------------// Week-3-assessment-Jatded1d9l7 -Code Challenges- -Question 1-
 
-// describe("fibonacci", () => {
-//   test("returns array with accumulating sum", () => {
-//           expect(fibonacci(6).toEqual([1, 1, 2, 3, 5, 8]))
-//           expect(fibonacci(10).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]))
+describe("fibonacci", () => {
+  test("returns array with accumulating sum", () => {
+          expect(fibonacci(6).toEqual([1, 1, 2, 3, 5, 8]))
+          expect(fibonacci(10).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]))
           
-//   })
-// })
+  })
+})
 
-// const fibonacci = (number) => {
-//   let answerArray = [1, 1]
-//   for(let i=2; i<number; i++){
-//     answerArray.push(answerArray[i - 2] + answerArray[i - 1])
-//   }
-//   return answerArray
-// }
+const fibonacci = (number) => {
+  let answerArray = [1, 1]
+  for(let i=2; i<number; i++){
+    answerArray.push(answerArray[i - 2] + answerArray[i - 1])
+  }
+  return answerArray
+}
 
 //-------------------- CODE BLOCK END ----------------------// Week-3-assessment-Jatded1d9l7 -Code Challenges- -Question 1-
 
@@ -47,23 +47,23 @@
 
 //-------------------- CODE BLOCK START ----------------------// Week-3-assessment-Jatded1d9l7 -Code Challenges- -Question 2-
 
-    //  describe("onlyOdds", () => {
-    //   test("returns a new array of only odd numbers sorted from least to greatest", () => {
-    //     var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-    //     // Expected output: [-9, 7, 9, 199]
-    //     var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-    //     // Expected output: [-823, 7, 23]
-    //     expect(onlyOdds(fullArr1)).toEqual([-9, 7, 9, 199])
-    //     expect(onlyOdds(fullArr2)).toEqual([-823, 7, 23])
-    //   })
-    // })
+     describe("onlyOdds", () => {
+      test("returns a new array of only odd numbers sorted from least to greatest", () => {
+        var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+        // Expected output: [-9, 7, 9, 199]
+        var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        // Expected output: [-823, 7, 23]
+        expect(onlyOdds(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(onlyOdds(fullArr2)).toEqual([-823, 7, 23])
+      })
+    })
 
-    // const onlyOdds = (array) => {
-    //   let filteredArray = array.filter(value => {
-    //     return typeof value === "number" && value%2 !== 0
-    //   })
-    //   return filteredArray.sort((a, b) => a - b)
-    // }
+    const onlyOdds = (array) => {
+      let filteredArray = array.filter(value => {
+        return typeof value === "number" && value%2 !== 0
+      })
+      return filteredArray.sort((a, b) => a - b)
+    }
 
 
 //-------------------- CODE BLOCK END ----------------------// Week-3-assessment-Jatded1d9l7 -Code Challenges- -Question 2-
